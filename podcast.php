d38a443aeaac87727c428a8b73bfc374db9d949c
Farouk Charkas <farouk.charkas@gmail.com>
2018-10-28 13:15:23 -0400

    added in template rss feed

--- podcast.php
+++ podcast.php
@@ -1,1 +1,70 @@
-
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
+<rss xmlns:npr="https://www.npr.org/rss/" xmlns:nprml="https://api.npr.org/nprml" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+<channel>
+<title>NPR Politics Podcast</title>
+<link>https://www.npr.org/sections/politics/</link>
+<description>
+<![CDATA[
+The <em>NPR Politics Podcast</em> is where NPR's political reporters talk to you like they talk to each other. With weekly roundups and quick takes on news of the day, you don't have to keep up with politics to know what's happening. You just have to keep up with us.
+]]>
+</description>
+<copyright>Copyright 2015-2018 NPR - For Personal Use Only</copyright>
+<generator>NPR API RSS Generator 0.94</generator>
+<language>en-us</language>
+<itunes:new-feed-url>https://www.npr.org/rss/podcast.php?id=510310</itunes:new-feed-url>
+<itunes:summary>
+<![CDATA[
+The <em>NPR Politics Podcast</em> is where NPR's political reporters talk to you like they talk to each other. With weekly roundups and quick takes on news of the day, you don't have to keep up with politics to know what's happening. You just have to keep up with us.
+]]>
+</itunes:summary>
+<itunes:author>NPR</itunes:author>
+<itunes:block>no</itunes:block>
+<itunes:owner>
+<itunes:email>podcasts@npr.org</itunes:email>
+<itunes:name>NPR</itunes:name>
+</itunes:owner>
+<itunes:category text="News & Politics"/>
+<itunes:category text="Government & Organizations"/>
+<itunes:image href="https://media.npr.org/assets/img/2018/08/03/nprpolitics_tile_sq-ca364a35503019925f7cf384dfd923bf9bacf81c.jpg?s=1400"/>
+<itunes:type>episodic</itunes:type>
+<image>
+<url>
+https://media.npr.org/assets/img/2018/08/03/nprpolitics_tile_sq-ca364a35503019925f7cf384dfd923bf9bacf81c.jpg?s=1400
+</url>
+<title>NPR Politics Podcast</title>
+<link>https://www.npr.org/sections/politics/</link>
+</image>
+<lastBuildDate>Fri, 26 Oct 2018 17:14:46 -0400</lastBuildDate>
+<item>
+<title>
+Feds Charge Man With Mailing Improvised Bombs To President Trump's Critics
+</title>
+<description>
+<![CDATA[
+The Justice Department has charged Cesar Sayoc, 56, in connection with a wave of improvised explosive devices sent to political critics of President Trump. He faces a potential total of 48 years in prison. This episode: Congressional correspondent Scott Detrow, justice correspondent Carrie Johnson and national security editor Phil Ewing. Email the show at nprpolitics@npr.org. Find and support your local public radio station at npr.org/stations.
+]]>
+</description>
+<pubDate>Fri, 26 Oct 2018 17:14:46 -0400</pubDate>
+<copyright>Copyright 2015-2018 NPR - For Personal Use Only</copyright>
+<guid>61e04dab-e3bb-4e45-b649-1ddec4a849cf</guid>
+<itunes:title>
+Feds Charge Man With Mailing Improvised Bombs To President Trump's Critics
+</itunes:title>
+<itunes:author>NPR</itunes:author>
+<itunes:summary>
+<![CDATA[
+The Justice Department has charged Cesar Sayoc, 56, in connection with a wave of improvised explosive devices sent to political critics of President Trump. He faces a potential total of 48 years in prison. This episode: Congressional correspondent Scott Detrow, justice correspondent Carrie Johnson and national security editor Phil Ewing. Email the show at nprpolitics@npr.org. Find and support your local public radio station at npr.org/stations.
+]]>
+</itunes:summary>
+<itunes:duration>748</itunes:duration>
+<itunes:explicit>no</itunes:explicit>
+<itunes:episodeType>full</itunes:episodeType>
+<content:encoded>
+<![CDATA[
+The Justice Department has charged Cesar Sayoc, 56, in connection with a wave of improvised explosive devices sent to political critics of President Trump. He faces a potential total of 48 years in prison. This episode: Congressional correspondent Scott Detrow, justice correspondent Carrie Johnson and national security editor Phil Ewing. Email the show at nprpolitics@npr.org. Find and support your local public radio station at npr.org/stations.
+]]>
+</content:encoded>
+<enclosure url="https://play.podtrac.com/npr-510310/npr.mc.tritondigital.com/NPR_510310/media/anon.npr-mp3/npr/nprpolitics/2018/10/20181026_nprpolitics_politics_podcast_oct_26_bomb__-_upload_final.mp3?orgId=1&d=748&p=510310&story=661117103&t=podcast&e=661117103&ft=pod&f=510310" length="0" type="audio/mpeg"/>
+</item>
+</channel>
+</rss>
